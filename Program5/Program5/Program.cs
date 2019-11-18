e61b8993a75dedba9c9912697e6957ac6a4c02ac
jasonluong-cis <57879781+jasonluong-cis@users.noreply.github.com>
2019-11-17 20:36:12 -0500

    Add files via upload

---
+++ Program5/Program5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Program5
+{
+    public class TreeTest
+    {
+        static void Main()
+        {
+            int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.1 };
+            string[] stringArray = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+        }
+
+        Tree intTree = new Tree();
+        
+
+
+        private static void PopulateTree()
+        {
+
+        }
+    }
+}
---
+++ Program5/Program5/TreeNode.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program5
+{
+    class TreeNode 
+    {
+        public TreeNode LeftNode { get; set; }
+
+        public IComparable Data { get; private set; }
+
+        public TreeNode RightNode { get; set; }
+
+        public TreeNode(IComparable T) 
+        {
+            Data = T;
+        }
+
+        public void Insert(IComparable insertValue)
+        {
+            if (insertValue.CompareTo(Data) < 0)
+            {
+                if (LeftNode == null)
+                {
+                    LeftNode = new TreeNode(insertValue);
+                }
+                else
+                {
+                    LeftNode.Insert(insertValue);
+                }
+            }
+            else if (insertValue.CompareTo(Data) > 0)
+            {
+                if (RightNode == null)
+                {
+                    RightNode = new TreeNode(insertValue);
+                }
+                else
+                {
+                    RightNode.Insert(insertValue);
+                }
+            }
+        }
+    }
+
+    public class Tree 
+    {
+        private TreeNode root;
+
+        public void InsertNode(IComparable insertValue)
+        {
+            if (root == null)
+            {
+                root = new TreeNode(insertValue);
+            }
+            else
+            {
+                root.Insert(insertValue);
+            }
+        }
+
+        public void PreOrderTraversal()
+        {
+            PreorderHelper(root);
+        }
+        
+        private void PreorderHelper(TreeNode node)
+        {
+            if(node != null)
+            {
+                Console.Write($"{node.Data}");
+
+                PreorderHelper(node.LeftNode);
+
+                PreorderHelper(node.RightNode);
+            }
+        }
+
+        private void InorderTraversal()
+        {
+            InorderHelper(root);
+        }
+
+        private void InorderHelper(TreeNode node)
+        {
+            if (node != null)
+            {
+                InorderHelper(node.LeftNode);
+
+                Console.Write($"{node.Data}");
+
+                InorderHelper(node.RightNode);
+            }
+        }
+
+        public void PostorderTraversal()
+        {
+            PostorderHelper(root);
+        }
+
+        private void PostorderHelper(TreeNode node)
+        {
+            if(node != null)
+            {
+                PostorderHelper(node.LeftNode);
+
+                PostorderHelper(node.RightNode);
+
+                Console.Write($"{node.Data}");
+            }
+        }
+    }
+}
